bcafbc3a969936b24589694ade492e35849d6b47
Harsh Prasad <harshisindian@gmail.com>
2026-02-26 16:36:18 +0530

    Data Types and Type Checking

---
+++ src/Data Types.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var score: Int = 5
+    val temperature: Float = 0.0f
+    val isRaining: Boolean = true
+    val alphabet: Char = 'a'
+    val message: String = "Hello World"
+
+    // score = "abc"
+    // Assignment type mismatch: actual type is 'String', but 'Int' was expected.
+}
