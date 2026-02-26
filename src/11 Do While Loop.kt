b007361a19f2060136455100fb3a6f10911485e8
Harsh Prasad <harshisindian@gmail.com>
2026-02-26 23:10:08 +0530

    Do While Loop

---
+++ src/11 Do While Loop.kt
@@ -0,0 +1,24 @@
+fun main() {
+    val number = 2
+    var index = 1
+    do {
+        // This will get executed at least once.
+        println("$number * $index = ${number * index}")
+        index++
+    } while (index <= 10)
+    println("END")
+}
+
+// Output
+
+// 2 * 1 = 2
+// 2 * 2 = 4
+// 2 * 3 = 6
+// 2 * 4 = 8
+// 2 * 5 = 10
+// 2 * 6 = 12
+// 2 * 7 = 14
+// 2 * 8 = 16
+// 2 * 9 = 18
+// 2 * 10 = 20
+//END
