2e23c4f400f3f37f090a66b6f4d3461746ad7236
Harsh Prasad <harshisindian@gmail.com>
2026-02-26 16:20:00 +0530

    Print vs Print ln

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,10 @@
 fun main() {
-
+    println("Learning Kotlin Again With cursor on next line.")
+    print("Learning Kotlin Again With cursor on same line.")
+    print("End")
 }
+
+//Output
+
+//Learning Kotlin Again With cursor on next line.
+//Learning Kotlin Again With cursor on same line.End
