8381a9aaee0c0e601dab283f76174e844564357e
Harsh Prasad <harshisindian@gmail.com>
2026-02-26 16:49:25 +0530

    Operators

---
+++ src/04 Operators.kt
@@ -0,0 +1,31 @@
+fun main() {
+    val a = 20
+    val b = 7
+
+    // Arithmetic Operators
+    println(a + b) // 27
+    println(a - b) // 13
+    println(a * b) // 140
+    println(a / b) // 2.857143
+    println(a.toFloat() / b) // 2
+    println(a % b) // 6
+
+    // Relational Operators
+    println(a > b) // true
+    println(a < b) // false
+    println(a >= b) // true
+    println(a <= b) // false
+    println(a == b) // false
+    println(a != b) // true
+
+    // Increment / Decrement operator
+    var p = 10
+    var q = 10
+    var r = 10
+    var s = 10
+    println(p++) // 10
+    println(q--) // 10
+    println(++r) // 11
+    println(--s) // 9
+}
+
