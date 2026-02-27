88aec0fe2a77742e4392420562869f401008da38
Harsh Prasad <harshisindian@gmail.com>
2026-02-28 00:40:53 +0530

    Overriding in Inheritance

---
+++ src/Overriding in Inheritance.kt
@@ -0,0 +1,43 @@
+fun main() {
+    println("iPhone16 is being created")
+    val iPhone16 = Apple("iPhone 16")
+    iPhone16.display()
+    println()
+    println("ABC is being created")
+    val keypadPhone = Mobile("ABC")
+    keypadPhone.display()
+}
+
+open class Mobile(nameParam: String) {
+    init {
+        println("Mobile init is called")
+    }
+    val name: String = nameParam
+    open val size: Double = 5.5
+    fun makeCall() = println("Make call")
+    fun powerOff() = println("Power off")
+    open fun display() = println("LCD Display")
+}
+
+class Apple(nameParam: String): Mobile(nameParam) {
+    init {
+        println("Apple init is called")
+    }
+    override val size: Double = 6.1
+    override fun display() {
+        super.display() // Calls the parent display method
+        println("I have an OLED display")
+    }
+}
+
+// Output
+
+// iPhone16 is being created
+// Mobile init is called
+// Apple init is called
+// LCD Display
+// I have an OLED display
+//
+// ABC is being created
+// Mobile init is called
+// LCD Display
