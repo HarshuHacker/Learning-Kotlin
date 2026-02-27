89cd5e2e2e5a71064e3a4e04445e74ddf4b651cc
Harsh Prasad <harshisindian@gmail.com>
2026-02-28 00:44:47 +0530

    Any data type

--- src/03 Data Types.kt
+++ src/03 Data Types.kt
@@ -7,4 +7,12 @@ fun main() {
 
     // score = "abc"
     // Assignment type mismatch: actual type is 'String', but 'Int' was expected.
+
+    var anyDataType: Any =
+        123 // Any is the parent of all classes (int, string, double etc). So you can override `Any` with any type of data
+    println(anyDataType) // 123
+    anyDataType = "abc"
+    println(anyDataType) // abc
+    anyDataType = 12.24
+    println(anyDataType) // 12.24
 }
